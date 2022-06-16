677dc35536e92a1ba51867c2c4fa9870407859b7
pn496 <106802634+pn496@users.noreply.github.com>
2022-06-17 08:38:26 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,9 +10,63 @@ btn.addEventListener('click', () => {
   }
 });
 
-  /*-------------------------------------------
-  AOS animation
-  -------------------------------------------*/
-  AOS.init({
-    duration: 3000
+/*-------------------------------------------
+AOS animation
+-------------------------------------------*/
+AOS.init({
+  duration: 3000
+});
+$(function() {
+
+// 変数にクラスを入れる
+  var btn = $('.btn-menu');
+//スクロールしてページトップから1に達したらボタンを表示
+$(window).on('load scroll', function(){
+  if($(this).scrollTop() > 270) {
+  btn.addClass('active');
+  }else{
+  btn.removeClass('active');
+  }
   });
+});
+$(function() {
+  // 変数にクラスを入れる
+  var btn = $('.page_top');
+  //スクロールしてページトップから1に達したらボタンを表示
+  $(window).on('load scroll', function(){
+    if($(this).scrollTop() > 1) {
+    btn.addClass('active');
+    }else{
+    btn.removeClass('active');
+    }
+  });
+  //スクロールしてトップへ戻る
+  btn.on('click',function () {
+    $('body,html').animate({
+    scrollTop: 0
+    });
+  });
+});
+VANTA.CLOUDS({
+  el: "#header",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  speed: 0.65
+})
+VANTA.NET({
+  el: "#skills",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x25c841,
+  backgroundColor: 0x0,
+  maxDistance: 24.00,
+  spacing: 14.00
+})
